294aae15bccd33657c13694dec0f31df43130813
Gergox <Grigorenkosv23@gmail.com>
2017-09-24 16:37:38 +0300

    add new class user

---
+++ entity/User.java
@@ -0,0 +1,46 @@
+package entity;
+
+import javax.persistence.PrePersist;
+
+public class User {
+    private int age;
+    private String name;
+
+    public User() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return age == user.age && (name != null ? name.equals(user.name) : user.name == null);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = age;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
